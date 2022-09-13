5f8925748fe9647e31f34bde7882f97d56eb9c2a
khashayar shayesteh <shayesteh.office@gmail.com>
2022-09-13 08:03:26 -0700

    add container and configuration and wiring to  spring-05

--- spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/casefactory/Case.java
+++ spring-04-Autowiring/src/main/java/com/cydeo/streotype_annotation/casefactory/Case.java
@@ -1,6 +1,9 @@
 package com.cydeo.streotype_annotation.casefactory;
 
 import lombok.Getter;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.awt.*;
 
 @Getter
 public abstract class Case {
@@ -8,6 +11,9 @@ public abstract class Case {
     private String model;
     private String manufacturer;
     private String powerSupply;
+
+    @Autowired
+    private Dimension dimension;
 
     public Case(String model, String manufacturer, String powerSupply) {
         this.model = model;
