ce5ba5628ea20e5e3367cee619ea857e4a3e4ec4
khashayar shayesteh <shayesteh.office@gmail.com>
2022-09-12 19:40:17 -0700

    add container and configuration to spring-02

--- spring-00-Coupling/src/main/java/com/cydeo/loosely_coupled/casefactory/Case.java
+++ spring-00-Coupling/src/main/java/com/cydeo/loosely_coupled/casefactory/Case.java
@@ -1,16 +1,21 @@
 package com.cydeo.loosely_coupled.casefactory;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public abstract class Case {
 
     private String model;
     private String manufacturer;
     private String powerSupply;
 
-    public Case(String model, String manufacturer, String powerSupply) {
-        this.model = model;
-        this.manufacturer = manufacturer;
-        this.powerSupply = powerSupply;
-    }
+
+
+//    public Case(String model, String manufacturer, String powerSupply) {
+//        this.model = model;
+//        this.manufacturer = manufacturer;
+//        this.powerSupply = powerSupply;
+//    }
 
     public abstract void pressPowerButton();
 }
