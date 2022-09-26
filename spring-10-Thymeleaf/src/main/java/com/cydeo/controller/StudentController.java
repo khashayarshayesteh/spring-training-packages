453fbf92fd81e0e22982ee4a0c66aac6a83752b6
khashayar shayesteh <shayesteh.office@gmail.com>
2022-09-25 19:00:54 -0700

    Spring  Thymeleaf

---
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/StudentController.java
@@ -0,0 +1,26 @@
+package com.cydeo.controller;
+
+import com.cydeo.boostrap.DataGenerator;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+    @RequestMapping("/register") //localhost:8080/student/register
+    public String register(Model model){
+
+        model.addAttribute("students", DataGenerator.createStudent());
+
+        return "student/register";
+    }
+
+    @RequestMapping("/welcome") //localhost:8080/student/welcome?name=Ozzy
+    public String welcome(){
+
+
+        return "student/welcome";
+    }
+}
--- spring-10-Thymeleaf/src/main/java/com/cydeo/model/Student.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/model/Student.java
@@ -1,8 +1,10 @@
 package com.cydeo.model;
 
 import lombok.AllArgsConstructor;
+import lombok.Data;
 
 @AllArgsConstructor
+@Data
 public class Student {
     private String firstName;
     private String lastName;
