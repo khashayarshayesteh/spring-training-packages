2e6dc5e18360df076193a74c628d2d6d0367506c
khashayar shayesteh <shayesteh.office@gmail.com>
2022-09-26 23:03:11 -0700

    Spring  Thymeleaf

--- spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
+++ spring-10-Thymeleaf/src/main/java/com/cydeo/controller/MentorController.java
@@ -4,7 +4,12 @@ import com.cydeo.model.Mentor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @RequestMapping("/mentor")
@@ -13,6 +18,8 @@ public class MentorController {
     @GetMapping("/register")
     public String register(Model model){
 
+        List<String> batchList = Arrays.asList("JD1","JD2","JD3");
+        model.addAttribute("batchList",batchList);
         model.addAttribute("mentor", new Mentor());
         return "mentor/mentor-register";
     }
@@ -20,5 +27,15 @@ public class MentorController {
 //    public String drop(){
 //
 //        return "student/register";
+//    }
+    @PostMapping("/confirm")
+    public String submitForm(@ModelAttribute("mentor")Mentor mentor){
+        // i access to "mentor" attribute in this method with @ModelAttribute
+        return  "mentor/mentor-confirmation";
+    }
+
+//    @GetMapping("/confirm")
+//    public String submitForm2(){
+//        return  "mentor/mentor-confirmation";
 //    }
 }
