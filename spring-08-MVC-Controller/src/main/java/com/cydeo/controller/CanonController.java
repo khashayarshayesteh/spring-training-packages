70ad28dd05dd422e070f33241ebb2e28414cb7aa
khashayar shayesteh <shayesteh.office@gmail.com>
2022-10-16 21:40:51 -0700

    Spring  Thymeleaf

--- spring-08-MVC-Controller/src/main/java/com/cydeo/controller/CanonController.java
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/controller/CanonController.java
@@ -2,11 +2,13 @@ package com.cydeo.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
 @Controller
 public class CanonController {
-    @RequestMapping("canon")
-    public String canon(){
+
+    @RequestMapping("/canon")
+    public String getMyEmployer(){
         return "canon.html";
     }
+
+
 }
