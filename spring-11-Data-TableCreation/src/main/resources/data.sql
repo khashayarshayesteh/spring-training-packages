b3fa77197c5be7c927425dab4b095172cedc8b90
khashayar shayesteh <shayesteh.office@gmail.com>
2022-10-23 19:02:58 -0700

    application.properties

--- spring-11-Data-TableCreation/src/main/resources/data.sql
+++ spring-11-Data-TableCreation/src/main/resources/data.sql
@@ -1,8 +1,8 @@
 INSERT INTO EMPLOYEES(id, name)
 VALUES (1,'Mike');
 
-INSERT INTO students( first_name, last_name, email)
+INSERT INTO student( first_name, last_name, email)
 VALUES ('Mike', 'smith', 'mike@cydeo.com');
 
-INSERT INTO students(first_name, last_name, email)
+INSERT INTO student(first_name, last_name, email)
 VALUES ('Tom', 'Hank', 'tom@cydeo.com');
