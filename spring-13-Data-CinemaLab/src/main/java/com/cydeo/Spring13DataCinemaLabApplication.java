5ec1504560b97b075a7acad1ff5daf3fe4304440
khashayar shayesteh <shayesteh.office@gmail.com>
2022-10-26 01:10:48 -0700

    CinemaLab

--- spring-13-Data-CinemaLab/src/main/java/com/cydeo/Spring13DataCinemaLabApplication.java
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/Spring13DataCinemaLabApplication.java
@@ -17,7 +17,7 @@ public class Spring13DataCinemaLabApplication {
         SpringApplication.run(Spring13DataCinemaLabApplication.class, args);
     }
 
-    @Bean
-    public MigrateResult migrateResult(DataSource dataSource){return Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
-    }
+//    @Bean
+//    public MigrateResult migrateResult(DataSource dataSource){return Flyway.configure().baselineOnMigrate(true).dataSource(dataSource).load().migrate();
+//    }
 }
