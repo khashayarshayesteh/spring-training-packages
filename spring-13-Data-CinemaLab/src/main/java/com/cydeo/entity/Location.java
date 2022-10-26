315e24b73a03ecc9e237402b3ef6a44bb2b0913c
khashayar shayesteh <shayesteh.office@gmail.com>
2022-10-25 21:47:30 -0700

    CinemaLab

---
+++ spring-13-Data-CinemaLab/src/main/java/com/cydeo/entity/Location.java
@@ -0,0 +1,17 @@
+package com.cydeo.entity;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Location extends BaseEntity{
+
+
+
+
+
+}
