0e38a6e89a0ce3044877cdbc8f073c190970d78d
khashayar shayesteh <shayesteh.office@gmail.com>
2022-11-27 09:16:57 -0800

    spring-16

--- spring-16-REST-Jackson/src/main/java/com/cydeo/Spring16RestJacksonApplication.java
+++ spring-16-REST-Jackson/src/main/java/com/cydeo/Spring16RestJacksonApplication.java
@@ -6,13 +6,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Spring15RestApplication {
+public class Spring16RestJacksonApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring15RestApplication.class, args);
+        SpringApplication.run(Spring16RestJacksonApplication.class, args);
     }
 
     @Bean
-    public ModelMapper mapper() {return new ModelMapper();}
+    public ModelMapper mapper() {
+        return new ModelMapper();
+    }
 
 }
